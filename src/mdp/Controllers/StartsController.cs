ded1e1db57ad4963435600aead17a0648f43ca18
Expiers1 <dobudoglo.maxim@gmail.com>
2020-06-12 15:54:19 +0300

    vse

--- src/mdp/Controllers/StartsController.cs
+++ src/mdp/Controllers/StartsController.cs
@@ -20,31 +20,35 @@ namespace mdp.Controllers
         }
 
         // GET: Starts
-        public ActionResult Starts(ProductSearch obj)
+       public ActionResult Starts(ProductSearch obj)
         {
             if (obj.ProductCategory >= 0)
             {
                 // perforn search by product category
                 var prods_category =
-                    DBContext.Products.Where(p => p.CategoryId == (CategoryType)obj.ProductCategory).ToList();
+                    DBContext.Products.Include(a => a.Comments)
+                                      .Include(c => c.Images)
+                                      .Where(p => p.CategoryId == (CategoryType)obj.ProductCategory).ToList();
 
                 return View(prods_category);
             }
             else if (string.IsNullOrEmpty(obj.SearchText))
             {
                 // perform search for newest products and provide to start page
-                var prods_newest = (from p in DBContext.Products
-                             orderby p.DateAdded descending
-                             select p).Take(10).ToList();
+
+                var prods_newest = DBContext.Products.Include(a => a.Comments)
+                                                     .Include(c => c.Images)
+                                                     .OrderByDescending(p => p.DateAdded)
+                                                     .Take(10).ToList();
 
                 return View(prods_newest);
             }
 
             // perforn search by text
-            var prods_search = (from p in DBContext.Products
-                         orderby p.DateAdded descending
-                                where p.ProductName.Contains(obj.SearchText) || p.Description.Contains(obj.SearchText)
-                         select p).ToList();
+            var prods_search = DBContext.Products.Include(a => a.Comments)
+                                                     .Include(c => c.Images)
+                                                     .Where(p => p.ProductName.Contains(obj.SearchText) || p.Description.Contains(obj.SearchText))
+                                                     .OrderByDescending(p => p.DateAdded).ToList();
 
             return View(prods_search);
         }
